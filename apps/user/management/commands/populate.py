ccbcd65107eeb594c85fcceae00f89d339655195
Zakir Aghayev <agayevzakir239@gmail.com>
2024-02-28 13:49:33 +0400

    Done with populate user

---
+++ apps/user/management/commands/populate.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = 'Runs all populate commands to set up the database.'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS(
+            'Starting database population...'))
+
+        # Call populate_groups command
+        self.stdout.write(self.style.SUCCESS('Populating groups...'))
+        call_command('populate_groups')
+
+        # Call populate_staff_users command
+        self.stdout.write(self.style.SUCCESS('Populating staff users...'))
+        call_command('populate_staff_users')
+
+        self.stdout.write(self.style.SUCCESS('Database population complete.'))
