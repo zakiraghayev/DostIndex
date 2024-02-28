74bc7b9abf1cd38619287cf51a001ba207795eef
Zakir Aghayev <agayevzakir239@gmail.com>
2024-02-28 14:34:12 +0400

    Admin added assessment

---
+++ apps/assessment/admin/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa: F401
+
+from apps.assessment.admin.article import Article
+from apps.assessment.admin.article import Section
+
+from apps.assessment.admin.assessment import Assessment
+from apps.assessment.admin.assessment import AssessmentPoint
+
+from apps.assessment.admin.dost_center import DostCenter
